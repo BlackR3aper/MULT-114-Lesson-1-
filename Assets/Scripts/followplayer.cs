04ec16957e927bb783f1503c8b9fcd0610a097f7
Donggeon (Logan) Lee <beatsbydre8088@gmail.com>
2020-02-18 23:53:00 -0800

    Update

---
+++ Assets/Scripts/followplayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followplayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 7, -10);
+    //Camera angle for the player. 3rd person view
+
+
+    // Comment
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
